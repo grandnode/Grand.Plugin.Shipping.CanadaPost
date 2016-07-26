48f4124cba713c936b3015e2599b9fd5facd0043
rafal kalisz <rafal.kalisz.dev@gmail.com>
2016-07-26 16:54:09 +0200

    init

---
+++ CanadaPostSettings.cs
@@ -0,0 +1,14 @@
+﻿
+using Grand.Core.Configuration;
+
+namespace Grand.Plugin.Shipping.CanadaPost
+{
+    public class CanadaPostSettings : ISettings
+    {
+        public string Url { get; set; }
+
+        public int Port { get; set; }
+
+        public string CustomerId { get; set; }
+    }
+}
---
+++ Models/CanadaPostShippingModel.cs
@@ -0,0 +1,16 @@
+﻿using Grand.Web.Framework;
+
+namespace Grand.Plugin.Shipping.CanadaPost.Models
+{
+    public class CanadaPostShippingModel
+    {
+        [NopResourceDisplayName("Plugins.Shipping.CanadaPost.Fields.Url")]
+        public string Url { get; set; }
+
+        [NopResourceDisplayName("Plugins.Shipping.CanadaPost.Fields.Port")]
+        public int Port { get; set; }
+
+        [NopResourceDisplayName("Plugins.Shipping.CanadaPost.Fields.CustomerId")]
+        public string CustomerId { get; set; }
+    }
+}
